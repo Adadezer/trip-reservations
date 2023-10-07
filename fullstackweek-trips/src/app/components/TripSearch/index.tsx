c9fbb0333d8b4aeff53c8e891693db69141dff3f
adadezer <adadezer@gmail.com>
2023-10-07 18:46:05 -0300

    feat: :sparkles: input de data para pesquisa de viagens

--- fullstackweek-trips/src/app/components/TripSearch/index.tsx
+++ fullstackweek-trips/src/app/components/TripSearch/index.tsx
@@ -1,3 +1,4 @@
+import DatePicker from '@/components/DatePicker';
 import Input from '@/components/Input';
 import React from 'react'
 
@@ -7,7 +8,14 @@ function TripSearch() {
       <h1 className="font-semibold text-2xl text-primaryDarker text-center">
         Encontre sua próxima <span className='text-primary'>viagem!</span>
       </h1>
-      <Input placeholder='Onde você quer ir?' />
+      <div className='flex flex-col gap-4 mt-5'>
+        <Input placeholder='Onde você quer ir?' />
+
+        <div className='flex gap-4'>
+          <DatePicker placeholderText='Data de ida' onChange={() =>{}} className='w-full'/>
+
+        </div>
+      </div>
     </div>
   )
 }
