61bdc0dc93abe4437aea5bbb368f5e143f38e890
adadezer <adadezer@gmail.com>
2023-10-10 16:27:14 -0300

    feat: :sparkles: link para detalhes da reserva, e header do componente detalhes da reserva

---
+++ fullstackweek-trips/src/app/trips/[tripId]/page.tsx
@@ -0,0 +1,29 @@
+import { prisma } from '@/lib/prisma';
+import React from 'react'
+import TripHeader from './components/TripHeader';
+
+const getTripDetails = async (tripId: string) => {
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId,
+    }
+  });
+
+  return trip;
+}
+
+async function TripDetails({params}: {params: {tripId: string}}) {
+  const trip = await getTripDetails(params.tripId);
+
+  if (!trip) return null;
+
+  return (
+    <div className='container mx-auto'>
+      <TripHeader trip={trip}/>
+
+      {/* RESERVA */}
+    </div>
+  )
+}
+
+export default TripDetails
