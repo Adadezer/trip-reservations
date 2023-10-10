8a522453fae5a44dc203b9ee311a736cf882eefb
adadezer <adadezer@gmail.com>
2023-10-10 17:17:06 -0300

    feat: :sparkles: adição componente TripDescription

--- fullstackweek-trips/src/app/trips/[tripId]/page.tsx
+++ fullstackweek-trips/src/app/trips/[tripId]/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma';
 import React from 'react'
 import TripHeader from './components/TripHeader';
 import TripReservation from './components/TripReservation';
+import TripDescription from './components/TripDescription';
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -20,8 +21,9 @@ async function TripDetails({params}: {params: {tripId: string}}) {
 
   return (
     <div className='container mx-auto'>
-      <TripHeader trip={trip}/>
-      <TripReservation trip={trip}/>
+      <TripHeader trip={trip} />
+      <TripReservation trip={trip} />
+      <TripDescription description={trip.description} />
     </div>
   )
 }
