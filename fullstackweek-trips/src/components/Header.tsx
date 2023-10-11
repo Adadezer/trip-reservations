ee4b20ad111a1c2a2cb0ddcfa2639fe40c6424df
adadezer <adadezer@gmail.com>
2023-10-11 12:04:46 -0300

    fix: :bug: botão logout no header em detalhes da viagem

--- fullstackweek-trips/src/components/Header.tsx
+++ fullstackweek-trips/src/components/Header.tsx
@@ -47,7 +47,7 @@ function Header() {
           />
 
           {menuIsOpen && (
-            <div className='absolute top-14 left-0 w-full h-full bg-white rounded-full shadow-md flex flex-col justify-center items-center'>
+            <div className='z-50 absolute top-14 left-0 w-full h-full bg-white rounded-full shadow-md flex flex-col justify-center items-center'>
               <button
                 className='text-primary text-sm font-semibold'
                 onClick={handleLogoutClick}
