2dbb9785fac5cd0ee1091ec054de105a266e89a0
adadezer <adadezer@gmail.com>
2023-10-25 17:25:41 -0300

    feat: :sparkles: criação da reserva no banco de dados

--- fullstackweek-trips/src/app/trips/[tripId]/confirmation/page.tsx
+++ fullstackweek-trips/src/app/trips/[tripId]/confirmation/page.tsx
@@ -9,6 +9,7 @@ import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import ReactCountryFlag from 'react-country-flag';
+import { toast } from 'react-toastify';
 
 function TripConfirmation({params}: {params: {tripId: string}}) {
   const [trip, setTrip] = useState<Trip | null>();
@@ -16,7 +17,7 @@ function TripConfirmation({params}: {params: {tripId: string}}) {
 
   const router = useRouter();
   
-  const { status } = useSession();
+  const { status, data } = useSession();
 
   const searchParams = useSearchParams();
   
@@ -39,7 +40,6 @@ function TripConfirmation({params}: {params: {tripId: string}}) {
 
       setTrip(res.trip);
       setTotalPrice(res.totalPrice);
-
     }
 
     if(status === 'unauthenticated') {
@@ -50,6 +50,29 @@ function TripConfirmation({params}: {params: {tripId: string}}) {
   }, [status]);
 
   if (!trip) return null;
+
+  const handleBuyClick = async () => {
+    const res = await fetch("http://localhost:3000/api/trips/reservation", {
+      method: "POST",
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get("startDate"),
+          endDate: searchParams.get("endDate"),
+          guests: Number(searchParams.get("guests")),
+          userId: (data?.user as any)?.id!,
+          totalPaid: totalPrice,
+        })
+      ),
+    });
+
+    if(!res.ok){
+      toast.error('Ocorreu um erro ao realizar a reserva', { position: 'top-center' });
+    }
+
+    router.push('/');
+    toast.success('Reserva realizada com sucesso!', { position: 'top-center' });
+  };
 
   const startDate = new Date(searchParams.get('startDate') as string);
   const endDate = new Date(searchParams.get('endDate') as string);
@@ -105,7 +128,7 @@ function TripConfirmation({params}: {params: {tripId: string}}) {
         <h3 className='font-semibold mt-5'>Hóspedes</h3>
         <p>{guests} hóspedes</p>
 
-        <Button className='mt-5'>Finalizar Compra</Button>
+        <Button className='mt-5' onClick={handleBuyClick}>Finalizar Compra</Button>
       </div>
     </div>
   );
