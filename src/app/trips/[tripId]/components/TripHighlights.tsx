262d1ca053c9cca9efe29715d24b0726e587e62e
adadezer <adadezer@gmail.com>
2023-11-10 16:44:43 -0300

    fix: :bug: id trip highlight

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 interface TripHighlightProps {
   highlights: string[];
+  tripId: string;
 }
 
 function TripHighlights({highlights}: TripHighlightProps) {
@@ -11,10 +12,10 @@ function TripHighlights({highlights}: TripHighlightProps) {
     <h2 className='font-semibold text-primaryDarker mb-2'>Destaques</h2>
     
     <div className='flex flex-wrap gap-y-3'>
-      {highlights.map((highlight, index) => (
-        <div key={index} className='flex items-center gap-2 w-1/2'>
+      {highlights.map((highlight, tripId) => (
+        <div key={tripId} className='flex items-center gap-2 w-1/2'>
           <Image src='/check-icon.png' width={15} height={15} alt={`imagem ${highlight}`} />
-          <p key={index} className='text-grayPrimary text-xs'>{highlight}</p>
+          <p key={tripId} className='text-grayPrimary text-xs'>{highlight}</p>
         </div>
       ))}
     </div>
