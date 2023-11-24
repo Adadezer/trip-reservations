8eebd1ec1c799d3c8abfcea5b0c14c5816108a5f
adadezer <adadezer@gmail.com>
2023-11-24 16:45:47 -0300

    style: :lipstick: mudança logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='flex flex-col justify-center items-center bg-walterWhite p-5'>
-      <Image src='/Logo.png' width={133} height={23} alt="logo Full Stack Week" />
+      <Image src='/Trip_Logo.png' width={40} height={15} alt="logo Full Stack Week" />
       <p className='text-sm font-medium mt-1 text-primaryDarker'>Todos os direitos reservados</p>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,43 +22,54 @@ function Header() {
 
   return (
     <div className="container mx-auto p-5 py-0 h-[92px] flex justify-between items-center shadow-lg lg:border-b lg:border-grayLighter">
-      <Link href='/'>
-        <div className='relative w-[183px] h-[32px]'>
-          <Image src="/Logo.png" alt="logo Full Stack Week" fill />
+      <Link href="/">
+        <div className="flex items-center flex-row-reverse gap-2">
+          <div className="font-bold text-primary">RESERVA DE VIAGENS</div>
+          <div className="relative w-[80px] h-[80px]">
+            <Image src="/Trip_Logo.png" alt="logo Full Stack Week" fill />
+          </div>
         </div>
       </Link>
 
-      {status === 'unauthenticated' &&
-        (
-          <div className='bg-white rounded-full p-3 px-5 border border-grayLighter shadow-md flex flex-col justify-center items-center'>
-            <button className='text-primary text-sm font-semibold' onClick={handleLoginClick}>
-              Login
-            </button>
-          </div>
-        )
-      }
+      {status === "unauthenticated" && (
+        <div className="bg-white rounded-full p-3 px-5 border border-grayLighter shadow-md flex flex-col justify-center items-center">
+          <button
+            className="text-primary text-sm font-semibold"
+            onClick={handleLoginClick}
+          >
+            Login
+          </button>
+        </div>
+      )}
 
-      {status === 'authenticated' && data.user &&(
+      {status === "authenticated" && data.user && (
         <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 px-3 relative">
-          <AiOutlineMenu size={16} onClick={handleMenuClick} className="cursor-pointer" />
+          <AiOutlineMenu
+            size={16}
+            onClick={handleMenuClick}
+            className="cursor-pointer"
+          />
 
           <Image
             width={34}
             height={34}
-            className='rounded-full'
+            className="rounded-full"
             src={data.user.image!}
             alt={data.user.name!}
           />
 
           {menuIsOpen && (
-            <div className='z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center'>
-              <Link href={'/my-trips'} onClick={() => setMenuIsOpen(false)}>
-                <button className='text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold mx-2'>
+            <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <Link href={"/my-trips"} onClick={() => setMenuIsOpen(false)}>
+                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold mx-2">
                   Minhas viagens
-                </button>             
+                </button>
               </Link>
 
-              <button className='text-primary text-sm font-semibold mt-2' onClick={handleLogoutClick}>
+              <button
+                className="text-primary text-sm font-semibold mt-2"
+                onClick={handleLogoutClick}
+              >
                 Logout
               </button>
             </div>
@@ -66,7 +77,7 @@ function Header() {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 export default Header
